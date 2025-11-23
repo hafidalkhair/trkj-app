8f1add9725aacb803110760c743f21cdfa38d414
hafidalkhair <hafidalkhair27@gmail.com>
2025-11-24 01:50:57 +0700

    email

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -58,7 +58,7 @@
                     </li>
                     <li class="flex items-center">
                         <svg class="w-5 h-5 text-teal-500 mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"/></svg>
-                        <a href="trkjcopium@gmail.com" class="hover:text-teal-600 transition-colors">trkjcopium@gmail.com</a>
+                        <a href="mailto:trkjcopium@gmail.com" class="hover:text-teal-600 transition-colors">trkjcopium@gmail.com</a>
                     </li>
                 </ul>
             </div>
